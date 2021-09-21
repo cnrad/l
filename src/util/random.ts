febca3ae819d2389dec26fc692e3c1911c0a7475
cnrad <83192247+cnrad@users.noreply.github.com>
2021-09-20 21:10:27 -0400

    add redis + basic pkgs

---
+++ src/util/random.ts
@@ -0,0 +1,13 @@
+export default function random(Length: number) {
+    let final: string = "";
+    const randomChar = () => {
+        let num = Math.floor(Math.random() * 62);
+        if (num < 10) return num; //1-10
+        if (num < 36) return String.fromCharCode(num + 55); //A-Z
+        return String.fromCharCode(num + 61); //a-z
+    }
+
+    while(final.length < Length) final += randomChar();
+
+    return final;
+}
