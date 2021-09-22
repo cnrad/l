1e828f25bac2e8bf98340e9237b344e8b3cae668
cnrad <83192247+cnrad@users.noreply.github.com>
2021-09-21 21:45:03 -0400

    fetch from redis and redirect link

---
+++ pages/[...link].tsx
@@ -0,0 +1,52 @@
+import redis from "../src/util/redis";
+import styled from 'styled-components';
+
+export async function getServerSideProps(context: any) {
+
+    const shortened = context.params.link;
+
+    let response = await redis.hget("links", shortened);
+
+    // await redis.hset("links") ip and user agent
+
+    if (!response) {
+      return {
+        notFound: true,
+      }
+    }
+  
+    return {
+        redirect: {
+          destination: response,
+          permanent: true,
+        },
+    }
+
+
+}
+  
+export default function Home({error, message}: any) {
+    return (
+        <Main>
+            <Message>Redirecting shortly...</Message>
+        </Main>
+    )
+}
+
+const Main = styled.div`
+    overflow: none;
+    width: 100%;
+    height: 100vh;
+    background: #000;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Message = styled.div`
+    font-size: 20px;
+    font-family: 'Inter', sans-serif;
+    font-weight: 600;
+    color: #fff;
+`
