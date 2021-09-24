e0d2d4c99dca32926f822d557213c66d4d409e5c
cnrad <83192247+cnrad@users.noreply.github.com>
2021-09-23 21:12:40 -0400

    frontend stuff not final

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,54 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Home: NextPage = () => {
 
+    let [cmdHistory, setCmdHistory]: any[] = useState([]);
+    let [command, setCommand] = useState("");
+
     useEffect(() => {
-        window.addEventListener('keypress', (event) => {
-            if (event.key === 'Enter') return handleCommand()
-        })
-    })
+        window.addEventListener('keypress', checkKey);
+    }, [cmdHistory])
+
+    const checkKey = (event: any): any => {
+        if (event.key === 'Enter') {
+            window.removeEventListener('keypress', checkKey);
+            handleCommand();
+            window.addEventListener('keypress', checkKey);
+        }
+    }
 
     const handleCommand = () => {
-        let commandText = (document.querySelector('#input') as HTMLInputElement).value;
-        console.log(commandText)
+
+        console.log(command);
+
+        let firstText = document.getElementById(`currentFirst`)!.innerText;
+        let commandText = command;
+        (document.getElementById(`currentCommand`) as HTMLInputElement).value = "";
+
+        setCmdHistory([...cmdHistory, {first: firstText, input: commandText}])
+
+        return console.log(cmdHistory);
+
     }
 
     return (
         <Page>
+            {cmdHistory.length > 0 ? cmdHistory.map((element: any, index: number) => {
+                return (
+                    <CommandLine key={`command${index}`}> 
+                        <First id={`first${index}`}>{element.first}</First>
+                        <PrevInput id={`input${index}`}>{element.input}</PrevInput>
+                    </CommandLine>
+                )
+            }) : ''}
+
             <CommandLine> 
-                {'cnrad/projects/l.cnrad.dev>'} 
-                <Input id="input"></Input>
+                <First id='currentFirst'>{'cnrad/projects/l.cnrad.dev>'}</First>
+                <Input id='currentCommand' onChange={e => setCommand(e.target.value)} />
             </CommandLine>
         </Page>
     )
@@ -44,8 +71,27 @@ const CommandLine = styled.div`
     font-family: 'Ubuntu Mono';
     font-size: 1rem;
 
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+
+    width: 100%;
+    height: 2rem;
+`
+
+const First = styled.div`
+    background: #000;
+    color: inherit;
+    outline: none;
+    border: none;
+    letter-spacing: 0.02rem;
+    font-family: 'Ubuntu Mono';
+    font-size: 1rem;
+
     width: auto;
     height: 2rem;
+    line-height: 2rem;
 `
 
 const Input = styled.input`
@@ -58,6 +104,21 @@ const Input = styled.input`
     font-size: 1rem;
 
     min-width: 60rem;
+    height: 2rem;
+    margin-left: 1rem;
+`
+
+const PrevInput = styled.div`
+    background: #000;
+    color: inherit;
+    outline: none;
+    border: none;
+    letter-spacing: 0.02rem;
+    font-family: 'Ubuntu Mono';
+    font-size: 1rem;
+    line-height: 2rem;
+
+    max-width: 60rem;
     height: 2rem;
     margin-left: 1rem;
 `
