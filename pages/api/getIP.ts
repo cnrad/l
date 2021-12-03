d7cc068497298fa7a07e2d9b872b0f8451682bcd
cnrad <conradwcrawford@gmail.com>
2021-12-02 20:44:09 -0500

    fix(getIP): try new method of getting IP

--- pages/api/getIP.ts
+++ pages/api/getIP.ts
@@ -1,15 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
     ip: string;
-}
+};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-
-    let ipAddr = req.headers["x-real-ip"] as string;
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    let ipAddr = (req.headers["x-forwarded-for"] ?? req.socket.remoteAddress ?? "unknown") as string;
 
     return res.status(200).json({ ip: ipAddr });
 }
